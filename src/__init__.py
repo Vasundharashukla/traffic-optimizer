18644663adaad2b1049fdf1a6bec8dcdfed5a40f
gagantalreja <gtcms1@gmail.com>
2020-10-14 21:39:46 +0530

    first

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from config import Config
+
+app = Flask(__name__, template_folder='./templates')
+app.config.from_object(Config)
+
+from src.routes import router
+app.register_blueprint(router)
