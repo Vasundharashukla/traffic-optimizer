66e6eac935f47f3a0a6ce0d7d6dec5b1c2c7815a
Hemant <17ucs066@lnmiit.ac.in>
2020-10-15 14:28:28 +0530

    navbaar

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from src import app
-
+from flask import Flask, render_template
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/index1/')
+def about():
+    return render_template('index1.html')
 if __name__ == "__main__":
     app.run(debug=True)
